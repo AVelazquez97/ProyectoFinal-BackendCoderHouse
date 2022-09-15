f4b7e78411fe8d1284effd306b1979563d7ea689
Alexis <alexisvp97@kali.0.2.15>
2022-09-15 20:17:01 -0300

    segunda entrega del proyecto sin terminar

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import { APP_PORT } from "./app/config/index.js";
 import { app } from "./app/app.js";
 
 /* ----------------------------- server settings ---------------------------- */
-const PORT = 8080 || APP_PORT; // Cambiar el orden cuando se haga deploy a Glitch
+const PORT = APP_PORT || 8080;
 app.listen(PORT, (error) => {
   if (error) throw `${error}`;
   console.log(`Servidor HTTP escuchando en el puerto ${PORT}`);
